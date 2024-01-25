e160f9967397e7c1d8be91ffd518f3fa614604c5
Haciibrahimli <haci.ibrahimli@gmail.com>
2024-01-25 20:26:44 +0200

    lastChanges

---
+++ my_app/signals.py
@@ -0,0 +1,9 @@
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from my_app.models import Product
+from django.shortcuts import redirect
+
+
+@receiver(post_save, sender=Product)
+def my_function(instance, created, **kwargs):
+    return 
